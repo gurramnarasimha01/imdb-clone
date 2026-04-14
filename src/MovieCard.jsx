96c8f1e8689de98fd0fb00b4bc7b81495b4dc494
Gurram Narasimha Naidu <narasimhanaidu.power123@gmail.com>
2026-04-14 16:51:11 +0530

    Create MovieCard component

---
+++ src/MovieCard.jsx
@@ -0,0 +1,14 @@
+function MovieCard({ poster, title, year, rating }) {
+     return (
+       <div style={{ width: "250px", border: "1px solid #ccc", borderRadius: "8px" }}>
+         <img src={poster} alt={title} style={{ width: "100%" }} />
+         <div style={{ padding: "10px" }}>
+           <h3>{title}</h3>
+           <p>Year: {year}</p>
+           <p>Rating: ⭐ {rating}</p>
+           //Task submission
+         </div>
+       </div>
+     );
+   }
+   export default MovieCard;
